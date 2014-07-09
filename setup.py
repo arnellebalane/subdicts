e1ab6ae91b2ccd01710c565c22633bc049197fd5
Arnelle Balane <arnellebalane@gmail.com>
2014-07-09 21:31:45 +0800

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,28 @@
+import os
 from setuptools import setup, find_packages
+
+
+def read(filename):
+    return open(os.path.join(os.path.dirname(__FILE__), filename)).read()
 
 setup(
     name = 'subdicts',
-    version = '0.1.0',
-    url = 'http://github.com/arnellebalane/subdicts',
+    version = '1.0.0',
+    url = 'https://github.com/arnellebalane/subdicts',
     license = 'MIT',
-    description = 'parses nested dictionay keys into sub-dictionaries',
+    description = 'A small python utility which parses nested-keys in dictionaries into sub-dictionaries',
+    long_description = read('README.md'),
     author = 'Arnelle Balane',
+    author_email = 'arnellebalane@gmail.com',
     packages = find_packages('src'),
     package_dir = {'': 'src'},
     install_requires = ['setuptools'],
-    test_suite = 'subdicts'
+    test_suite = 'subdicts',
+    classifiers = [
+        'Development Status :: 1 - Released',
+        'Intended Audience :: Developers',
+        'License :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python'
+    ]
 )

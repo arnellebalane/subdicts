8019dec6f7f435ff1f0442ca7d807f0a91f29c10
Arnelle Balane <arnellebalane@gmail.com>
2014-07-09 15:05:39 +0800

    edited license and added changes log

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'subdicts',
-    version = '0.1',
+    version = '0.1.0',
     url = 'http://github.com/arnellebalane/subdicts',
     license = 'MIT',
     description = 'parses nested dictionay keys into sub-dictionaries',

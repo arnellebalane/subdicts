40fdf39c8c42349b6a9baacf42761518119ca001
Arnelle Balane <arnellebalane@gmail.com>
2014-07-09 21:39:15 +0800

    fixed typo in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 
 def read(filename):
-    return open(os.path.join(os.path.dirname(__FILE__), filename)).read()
+    return open(os.path.join(os.path.dirname(__file__), filename)).read()
 
 setup(
     name = 'subdicts',

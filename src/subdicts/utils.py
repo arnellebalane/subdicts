6397d7bc715a44458a1e1e8611945e9476b5197d
Arnelle Balane <arnellebalane@gmail.com>
2014-07-11 16:58:39 +0800

    renamed dict param so as to not shadow builtin method

--- src/subdicts/utils.py
+++ src/subdicts/utils.py
@@ -1,21 +1,21 @@
 import re
 
 
-def parse(dict):
+def parse(dictionary):
     subd = {}
-    for key, value in dict.items():
+    for key, value in dictionary.items():
         keys = re.split('(\[|\])', key)
         keys = [x for x in keys if x not in ['[', ']', '']]
         subd = __insert(subd, keys, value)
     return subd
 
 
-def __insert(dict, keys, value):
+def __insert(dictionary, keys, value):
     if len(keys) == 1:
-        dict[keys[0]] = value
+        dictionary[keys[0]] = value
     else:
-        if dict.has_key(keys[0]):
-            dict[keys[0]] = __insert(dict[keys[0]], keys[1:], value)
+        if keys[0] in dictionary:
+            dictionary[keys[0]] = __insert(dictionary[keys[0]], keys[1:], value)
         else:
-            dict[keys[0]] = __insert({}, keys[1:], value)
-    return dict
+            dictionary[keys[0]] = __insert({}, keys[1:], value)
+    return dictionary
